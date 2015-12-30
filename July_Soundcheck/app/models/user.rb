d74f8f0776d11bc170e556dfd0cd2ed925e191d9
mathyoumore <mohr.73@osu.edu>
2015-12-30 16:11:24 -0500

    Starting work on User/Session

--- July_Soundcheck/app/models/user.rb
+++ July_Soundcheck/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
       has_many :reviews
+      attr_accessible :email, :password, :password_confirmation
+      validates_uniqueness_of :email
       has_secure_password
 end
