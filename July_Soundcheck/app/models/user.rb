4fc0618f7b1407f5fa1af6c456fc2eb54fdeb7c4
mathyoumore <mohr.73@osu.edu>
2016-01-11 19:07:23 -0500

    Added a possessive print form to user for later
    John -> John's
    Atticus -> Atticus'

--- July_Soundcheck/app/models/user.rb
+++ July_Soundcheck/app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
       has_many :reviews
       has_secure_password
       validates_uniqueness_of :email, :displayname
+
+      def possessive_print
+        displayname + ('s' == displayname[-1,1] ? "'" : "'s")
+      end
+
 end
